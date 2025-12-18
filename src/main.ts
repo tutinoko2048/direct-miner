6eb98f5415aa8e5f64ab9cc52408f5c85201c215
RetoRuto9900K <tutinoko2048@gmail.com>
2025-12-18 14:31:45 +0900

    feat: 対象範囲を設定できるように

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,35 @@
-import { world } from '@minecraft/server';
+import { Entity, system, world } from '@minecraft/server';
 
-world.afterEvents.worldLoad.subscribe(() => {
-  console.log('Hello world!');
+interface Settings {
+  'dm:use_manual_range': boolean;
+  'dm:range': number;
+}
+
+world.afterEvents.playerBreakBlock.subscribe((event) => {
+  const { block, player } = event;
+
+  const settings = world.getPackSettings() as unknown as Settings;
+  const { container } = player.getComponent('minecraft:inventory')!;
+
+  let entities: Entity[];
+  if (settings['dm:use_manual_range']) {
+    const range = settings['dm:range'];
+    entities = block.dimension.getEntities({
+      type: 'minecraft:item',
+      location: block.center(),
+      maxDistance: range,
+    });
+  } else {
+    entities = block.dimension.getEntitiesAtBlockLocation(block.location);
+  }
+
+  for (const entity of entities) {
+    if (!entity.isValid) continue;
+
+    const itemComponent = entity.getComponent('minecraft:item')
+    if (!itemComponent) continue;
+
+    container.addItem(itemComponent.itemStack);
+    entity.remove();
+  }
 });
